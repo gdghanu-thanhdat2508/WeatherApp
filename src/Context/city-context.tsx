32646338e529b7d9bbd4bebcc19c77c58203d1c3
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-04-07 23:04:42 +0700

    first commit

--- src/Context/city-context.tsx
+++ src/Context/city-context.tsx
@@ -1,19 +1,17 @@
+import { coordinate } from "@/types/weather.interface";
 import React, { createContext, useContext, useState } from "react";
 
-interface Coordinate {
-  lat: number;
-  lon: number;
-}
+
 
 interface CoordContextType {
-  coord: Coordinate;
-  setCoord: (coord: Coordinate) => void;
+  coord: coordinate;
+  setCoord: (coord: coordinate) => void;
 }
 
 const CoordContext = createContext<CoordContextType | undefined>(undefined);
 
 export const WeatherCoordProvider = ({ children }: { children: React.ReactNode }) => {
-  const [coord, setCoord] = useState<Coordinate>({ lat: 21.0245, lon: 105.8412 });
+  const [coord, setCoord] = useState<coordinate>({ lat: 21.0245, lon: 105.8412 });
 
   return (
     <CoordContext.Provider value={{ coord, setCoord }}>
